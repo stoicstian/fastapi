4dc376d0b6a6ee7ee1a44f6d5bdb3ceb8781b432
Sebastian Gutierrez <gutierrezgsebastian@outlook.com>
2020-09-05 16:13:00 -0500

    Creando api para encontrar horas global

---
+++ tutorial/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="./tutorial/static"), name="static")
+templates = Jinja2Templates(directory="./tutorial/templates")
